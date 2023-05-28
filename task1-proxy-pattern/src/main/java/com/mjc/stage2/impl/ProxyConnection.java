c034d8587248d7694094b6715384655c129559d4
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-05-28 19:15:47 +0600

    Second solution

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -4,26 +4,33 @@ package com.mjc.stage2.impl;
 import com.mjc.stage2.Connection;
 
 public class ProxyConnection implements Connection {
+    private boolean isClosed = true;
     private RealConnection realConnection;
     private ConnectionPool connectionPool = ConnectionPool.getInstance();
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
+        this.isClosed = true;
     }
 
     public void reallyClose() {
         // Write your code here!
         this.realConnection.close();
+        this.isClosed = false;
     }
     // Implement methods here!
 
     @Override
     public void close() {
-        connectionPool.releaseConnection(this);
+        if (this.isClosed) {
+            this.reallyClose();
+        } else {
+            connectionPool.releaseConnection(this);
+        }
     }
 
     @Override
     public boolean isClosed() {
-        return this.realConnection.isClosed();
+        return this.isClosed;
     }
 }
